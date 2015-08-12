f1f8a7cb1548351b110aa6088e080c728596ba0c
ysj1992 <9448443@qq.com>
2015-08-12 18:32:07 +0800

    首界面Demo

---
+++ Assets/Test/MainMapShake.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class MainMapShake : MonoBehaviour {
+	public float moveTime = 0.8f;
+	public float movePos = 90;
+	public float scaleMult = 1.2f;
+	public float scaleDuration = 0.8f;
+	public float shakeStrength = 10f;
+	public float shakeDuration = 0.5f;
+	public int shakeTime = 5;
+
+	// Use this for initialization
+	void Start () {
+		MapSnake ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void MapSnake()
+	{
+		transform.DOLocalMoveY (movePos, moveTime).From().OnComplete(()=>
+		                                                  {
+			transform.DOPunchScale(new Vector3(scaleMult, scaleMult, scaleMult), scaleDuration, 1, 0).OnComplete(()=>
+			{
+				transform.DOShakePosition(shakeDuration, shakeStrength, shakeTime, 90, false);
+			});
+		});
+	}
+}
